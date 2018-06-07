d201a065e283a3e527cc3393050fa031887a1692
rounakdatta <rounakdatta12@gmail.com>
2018-06-08 04:51:43 +0530

    reputation sys

--- app/views/web/public/js/interest.js
+++ app/views/web/public/js/interest.js
@@ -5,5 +5,8 @@ function simpleInterest(p, t, r) {
 function calcMyInterest(mydeposit, totaldeposit, fixedpc, fppc) {
 	let rem_money = totaldeposit - fixedpc - fppc;
 	var myIncentive = Math.exp((mydeposit - rem_money) / mydeposit);
-	return myIncentive;
+	var sum = rem_money + fppc;
+	return ((myIncentive / rem_money) * sum);
 }
+
+console.log("hello")
